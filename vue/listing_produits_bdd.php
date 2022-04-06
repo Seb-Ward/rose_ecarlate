0574af24a2c1ae44c93716203b366f41c8dbb231
Seb_Ward <seb.ward88@gmail.com>
2022-04-06 09:13:46 +0200

    Management of acces to certain pages only if admin

--- vue/listing_produits_bdd.php
+++ vue/listing_produits_bdd.php
@@ -1,8 +1,11 @@
 <?php
 session_start();
-if(isset($_SESSION['user'])){
+//if(isset($_SESSION['user'])){
 //var_dump($_SESSION ['user']);
-
+//}
+if(!isset($_SESSION["user"])){//si le user session n'est pas existant
+header("Location: ../vue/connexion_admin.php");
+die();//eviter que les robots chargent la page si on en a pas besoin
 }
 ?>
 
