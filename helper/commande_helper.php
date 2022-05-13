a587d02776e267e74a40085e75f97b74e7e2b989
Seb_Ward <seb.ward88@gmail.com>
2022-05-13 20:38:39 +0200

    creation of object Commande.php and Option_livraison.php and Taxe.php, and increation of new case helper(codeigniter) with file Commande.php

--- helper/commande_helper.php
+++ helper/commande_helper.php
@@ -3,5 +3,5 @@ include_once ("../entity/Commande.php");
 
 function transform_to_object($commande){
   $commandeobject= new Commande($commande['commande_ref, user, montant_total, date_commande, date_envoi, option_livraison']); 
-  $commandeobject->setCommande_ref();
+  
 }
